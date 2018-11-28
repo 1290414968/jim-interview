648e790a578a239298d6f8f165d34867ef5a4947
Administrator <1290414968@qq.com>
2018-11-28 22:54:11 +0800

    practice

---
+++ src/main/java/org/study/jim/interview/dataConstruct/linked/P3.java
@@ -0,0 +1,17 @@
+package org.study.jim.interview.dataConstruct.linked;
+/**
+ * @Auther: jim
+ * @Date: 2018/11/28 22:50
+ * @Description:
+ * 链表结构如下：1 8 3 6 5 4 7 2 9
+ * 进行排序
+ */
+public class P3 {
+    /**
+     * 实现步骤：
+     * 1、根据奇偶数进行拆分成两个链表
+     * 2、将偶数链表进行反转排序
+     * 3、将奇数链表和偶数链表进行合并
+     */
+
+}
